b941f35cc11ed4393e1a74b17814c692530136e3
Liviu Florescu <f_liviu@yahoo.com>
2024-09-03 20:53:41 +0200

    cleanup and style adjustments

---
+++ server_with_reload.py
@@ -0,0 +1,10 @@
+from livereload import Server, shell
+
+# Initialize the server
+server = Server()
+
+# Watch the current directory for changes
+server.watch(".", shell("python -m http.server"))
+
+# Start the server at the default port 8000
+server.serve(port=8000, host="0.0.0.0")
